7b6b33fec80cd679136afcc3ed67623df5f272ba
TTCAD\u-ri <u-ri@ttc-net.co.jp>
2018-04-25 18:34:44 +0900

    v0.1.1

--- csv.rb
+++ csv.rb
@@ -1,31 +1,17 @@
+#call the libraries
 require 'csv'
 require 'date'
 class Time_data
+  #add up the working time for each day of the week
   def data
-    csv_data = CSV.read('timesheet201804.csv')
-    total = [0, 0, 0, 0, 0, 0, 0]
-    csv_data = CSV.open("./timesheet201804.csv")
-    csv_data.readline
+    week = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"]
+    time = {"SUN":0, "MON":0, "TUE":0, "WED":0, "THU":0, "FRI":0, "SAT":0}
+    csv_data = CSV.read("./timesheet201804.csv",headers:true)
     csv_data.each do |row|
-        d = Date.parse row[0]
-        case d.wday
-        when 0
-          total[0] += row[3].to_f
-        when 1
-          total[1] += row[3].to_f
-        when 2
-          total[2] += row[3].to_f
-        when 3
-          total[3] += row[3].to_f
-        when 4
-          total[4] += row[3].to_f
-        when 5
-          total[5] += row[3].to_f
-        when 6
-          total[6] += row[3].to_f
-        end
+      time[:"#{week[Date.parse(row["ãŒñ±ì˙"]).wday]}"] += row["çÏã∆éûä‘"].to_f
     end
-    puts "SUN. #{total[0]}\nMON. #{total[1]}\nTUE. #{total[2]}\nWED. #{total[3]}\nTHU. #{total[4]}\nFRI. #{total[5]}\nSAT. #{total[6]}\n"
+    #output
+    p time
   end
 end
 csv=Time_data.new
--- number_game.rb
+++ number_game.rb
@@ -1,39 +1,22 @@
 class Number_game
+  #random a number as correct
   def initialize
     puts "数字を入力してください"
     @num = rand(0..100)
-    @flag_small = false
-    @flag_correct = false
+    #count the times you tried
     @cnt = 0
   end
-
+  #the main of this game
   def number_game
-    while true
+    #when the number inputted doesn't equal the correct
+    while @num_input != @num
+      #input a number
       @num_input = gets.chomp.to_i
       @cnt +=1
-      if @num_input == @num
-       @flag_correct = true
-       output
-       break
-      elsif @num_input > @num
-        @flag_small = false
-        output
-      elsif @num_input < @num
-        @flag_small = true
-        output
-      end
-    end
-  end
-
-  def output
-    if @flag_correct == false
-      if @flag_small == false
-        puts "#{@num_input}は正解より大きいです"
-      else
-        puts "#{@num_input}は正解より小さいです"
-      end
-    else
-      puts "正解です。正解まで#{@cnt}回答えました"
+      #judge if correct
+      puts "#{@num_input}は正解より大きいです" if @num_input > @num
+      puts "#{@num_input}は正解より小さいです" if @num_input < @num
+      puts "正解です。正解まで#{@cnt}回答えました" if @num_input == @num
     end
   end
 end
